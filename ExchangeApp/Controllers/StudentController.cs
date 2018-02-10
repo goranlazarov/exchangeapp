72ef81b1c05ffd62a625a26116d1eb1dccefb459
vpandev <pandev_vase@yahoo.com>
2018-02-10 19:25:09 +0100

    initial student view and controller

---
+++ ExchangeApp/Controllers/StudentController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ExchangeApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
