7431910015838763ac051f1fe96dbd069c1b2c87
unknown <goran_sr@hotmail.com>
2018-02-10 14:08:10 +0100

    q x
    

---
+++ ExchangeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExchangeApp.Startup))]
+namespace ExchangeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
