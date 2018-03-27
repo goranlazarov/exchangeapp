5f201e7510e60f1c1f166f7e8c1527ce3cf32fcb
Васе Пандев <pandev.vase@students.finki.ukim.mk>
2018-03-27 20:06:58 +0200

    Globalization added

--- ExchangeApp/Global.asax.cs
+++ ExchangeApp/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,18 @@ namespace ExchangeApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            CultureInfo cInf = new CultureInfo("en-US", false);
+
+            cInf.DateTimeFormat.DateSeparator = "/";
+            cInf.DateTimeFormat.ShortDatePattern = "MM/dd/yyyy";
+            cInf.DateTimeFormat.LongDatePattern = "MM/dd/yyyy hh:mm:ss tt";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = cInf;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = cInf;
         }
     }
 }
