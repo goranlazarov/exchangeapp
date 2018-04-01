cd411484b349273448ce4b41bd42a8fae9704071
goranlazarov <goran_sr@hotmail.com>
2018-04-01 19:45:01 +0200

    posledno postaveno

--- ExchangeApp/Helpers/MailSender.cs
+++ ExchangeApp/Helpers/MailSender.cs
@@ -82,9 +82,9 @@ namespace ExchangeApp.Helpers
                              .Replace("(UniversityFrom)", Model.UniversityFrom).Replace("(EmailAddress)", Model.Email)
                              .Replace("(HighestDegree)", Model.HighestDegree)
                              .Replace("(FirstCourse)", Model.FirstCourseName).Replace("(SecondCourse)", Model.SecondCourseName)
-                             .Replace("(ThirdCourse)", Model.ThirdCourseName).Replace("(FourthCourse)", Model.FourthCourseName);
+                             .Replace("(ThirdCourse)", Model.ThirdCourseName).Replace("(FourthCourse)", Model.FourthCourseName)
+                             .Replace("(Program)", Model.Faculty.Program);
 
-            //FacultyDegreeLevel i DegreeLevel nema
             return content;
 
         }
@@ -104,8 +104,6 @@ namespace ExchangeApp.Helpers
                              .Replace("(SemesterOfEnrollment)", Model.SemesterEnrolled)
                              .Replace("(UniversityFrom)", Model.UniversityFrom)
                              .Replace("(EmailAddress)", Model.Email);
-
-            // (UniversityCountry)
 
             return content;
 
